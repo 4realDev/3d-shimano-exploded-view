633eb87da3aaccb06394a139906a5835f852d368
Vladimir Brazhnik <v.brazhnik@web.de>
2021-10-15 20:44:55 +0200

    Remove snap state entirely and pass props by prop drilling (bad practise)

--- kkl-3d-prototype/src/App.js
+++ kkl-3d-prototype/src/App.js
@@ -4,12 +4,12 @@ import { Canvas } from '@react-three/fiber';
 import { Stage, OrbitControls } from '@react-three/drei';
 import { Suspense } from 'react';
 import Model from './components/models/PrototypeHouse';
-import { useSnapshot } from 'valtio';
-import { state } from './common/state';
 
 function App() {
-	const snap = useSnapshot(state);
 	const [meshList, setMeshList] = useState([]);
+	const [hoveredMesh, setHoveredMesh] = useState(null);
+	const [clickedMesh, setClickedMesh] = useState(null);
+	const [selectedMesh, setSelectedMesh] = useState(null);
 	return (
 		<div className='container'>
 			<div className='inner-container'>
@@ -18,12 +18,17 @@ function App() {
 						{/* create Loader UI as fallback before useLoader promise is returned */}
 						<Suspense fallback={null}>
 							{/* <Stage> will center and light the contents, create ground-shadows, and zoom the camera */}
-							<Stage
-								environment={null}
-								intensity={0.25}
-								contactShadowOpacity={0}
-							>
-								<Model meshList={meshList} setMeshList={setMeshList} />
+							<Stage environment={null} intensity={0.25} contactShadowOpacity={0}>
+								<Model
+									meshList={meshList}
+									setMeshList={setMeshList}
+									hoveredMesh={hoveredMesh}
+									setHoveredMesh={setHoveredMesh}
+									clickedMesh={clickedMesh}
+									setClickedMesh={setClickedMesh}
+									selectedMesh={selectedMesh}
+									setSelectedMesh={setSelectedMesh}
+								/>
 							</Stage>
 						</Suspense>
 						<OrbitControls />
@@ -31,9 +36,13 @@ function App() {
 				</div>
 			</div>
 			<div className='card-container'>
-				<div className='card-heading'>
-					<h1>{snap.current ? snap.current.replace('_', ' ') : ''}</h1>
-				</div>
+				<h1>Debug Console:</h1>
+				<p style={{ lineHeight: 0 }}>hovered: {hoveredMesh ? hoveredMesh : ''}</p>
+				<p style={{ lineHeight: 0 }}>clicked: {clickedMesh ? clickedMesh : ''}</p>
+				<p style={{ lineHeight: 0, marginBottom: 50 }}>selected: {selectedMesh ? selectedMesh : ''}</p>
+				{/* <div className='card-heading'>
+					<h1>{clickedMesh ? clickedMesh.replace('_', ' ') : ''}</h1>
+				</div> */}
 
 				<div className='product-button-container'>
 					{meshList.map((mesh) => {
@@ -41,7 +50,7 @@ function App() {
 							<button
 								className='product-button'
 								onClick={() => {
-									state.selected = mesh.name;
+									setSelectedMesh(mesh.name);
 								}}
 							>
 								{mesh.name.replace('_', ' ')}
@@ -51,7 +60,7 @@ function App() {
 					<button
 						className='product-button'
 						onClick={() => {
-							state.selected = null;
+							setSelectedMesh(null);
 						}}
 					>
 						RESET
