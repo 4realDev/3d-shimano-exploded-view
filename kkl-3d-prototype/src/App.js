b353d777dbb4fa88291262d80f13098962993363
Vladimir Brazhnik <v.brazhnik@web.de>
2021-10-15 19:58:26 +0200

    Enable basic ui functionality (hover, activate, transparent on button click)
    Problems:
    Calling every mesh with its name:
    nodes.inner_walls.geometry
    materials.inner_walls
    materials.inner_walls.name
    snap.items.inner_walls.opacity
    
    Hardcoding the name of the meshes for the comparison inside the Buttons
    Hardcoding the labels of the Buttons
    state.selected = 'outer_walls
    
    Bad maintenance when changing meshes or the entire 3D model

--- kkl-3d-prototype/src/App.js
+++ kkl-3d-prototype/src/App.js
@@ -3,30 +3,124 @@ import { Canvas } from '@react-three/fiber';
 import { Stage, OrbitControls } from '@react-three/drei';
 import { Suspense } from 'react';
 import Model from './components/models/PrototypeHouse';
+import { useSnapshot } from 'valtio';
+import { state } from './common/state';
 
 function App() {
+	const snap = useSnapshot(state);
 	return (
-		<div className='container'>
-			<Canvas
-				style={{ height: 100 + 'vh', width: 100 + '%' }}
-				camera={{ position: [0, -40, 0], fov: 40 }}
-			>
-				<spotLight
-					intensity={0.5}
-					angle={0.1}
-					penumbra={1}
-					position={[10, 15, 10]}
-					castShadow
-				/>
+		<div className='container' style={{ height: 50 + 'vh', width: 100 + '%' }}>
+			<Canvas camera={{ position: [0, -40, 0], fov: 40 }}>
 				{/* create Loader UI as fallback before useLoader promise is returned */}
 				<Suspense fallback={null}>
 					{/* <Stage> will center and light the contents, create ground-shadows, and zoom the camera */}
-					<Stage environment={null} intensity={0.25} contactShadowOpacity={0}>
+					<Stage intensity={0.25} contactShadowOpacity={0}>
 						<Model />
 					</Stage>
 				</Suspense>
 				<OrbitControls />
 			</Canvas>
+			<div className='card-container'>
+				<div className='card-heading'>
+					<h1>{snap.current ? snap.current.replace('_', ' ') : ''}</h1>
+				</div>
+
+				<div className='product-button-container'>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'stairs';
+							console.log(state.selected);
+						}}
+					>
+						Stairs
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'doors';
+							console.log(state.selected);
+						}}
+					>
+						Doors
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'outer_walls';
+							console.log(state.selected);
+						}}
+					>
+						Outer Walls
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'inner_walls';
+							console.log(state.selected);
+						}}
+					>
+						Inner Walls
+					</button>
+
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'floor';
+							console.log(state.selected);
+						}}
+					>
+						Floor
+					</button>
+				</div>
+				<div className='product-button-container'>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'windows';
+							console.log(state.selected);
+						}}
+					>
+						Window
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'roof';
+							console.log(state.selected);
+						}}
+					>
+						Roof
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'balcony';
+							console.log(state.selected);
+						}}
+					>
+						Balcony
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = 'fence';
+							console.log(state.selected);
+						}}
+					>
+						Fence
+					</button>
+					<button
+						className='product-button'
+						onClick={() => {
+							state.selected = null;
+							console.log(state.selected);
+						}}
+					>
+						Reset
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
