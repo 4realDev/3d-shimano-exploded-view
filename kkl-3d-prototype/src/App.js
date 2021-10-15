b93854c79c06c79e79273c5e42a109859115df70
Vladimir Brazhnik <v.brazhnik@web.de>
2021-10-15 20:05:31 +0200

    Create logic to map through gtlf and create json structured state for all meshes and buttons

--- kkl-3d-prototype/src/App.js
+++ kkl-3d-prototype/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Stage, OrbitControls } from '@react-three/drei';
 import { Suspense } from 'react';
@@ -8,116 +9,52 @@ import { state } from './common/state';
 
 function App() {
 	const snap = useSnapshot(state);
+	const [meshList, setMeshList] = useState([]);
 	return (
-		<div className='container' style={{ height: 50 + 'vh', width: 100 + '%' }}>
-			<Canvas camera={{ position: [0, -40, 0], fov: 40 }}>
-				{/* create Loader UI as fallback before useLoader promise is returned */}
-				<Suspense fallback={null}>
-					{/* <Stage> will center and light the contents, create ground-shadows, and zoom the camera */}
-					<Stage intensity={0.25} contactShadowOpacity={0}>
-						<Model />
-					</Stage>
-				</Suspense>
-				<OrbitControls />
-			</Canvas>
+		<div className='container'>
+			<div className='inner-container'>
+				<div className='canvas-wrapper'>
+					<Canvas camera={{ position: [0, -40, 0], fov: 40 }}>
+						{/* create Loader UI as fallback before useLoader promise is returned */}
+						<Suspense fallback={null}>
+							{/* <Stage> will center and light the contents, create ground-shadows, and zoom the camera */}
+							<Stage
+								environment={null}
+								intensity={0.25}
+								contactShadowOpacity={0}
+							>
+								<Model meshList={meshList} setMeshList={setMeshList} />
+							</Stage>
+						</Suspense>
+						<OrbitControls />
+					</Canvas>
+				</div>
+			</div>
 			<div className='card-container'>
 				<div className='card-heading'>
 					<h1>{snap.current ? snap.current.replace('_', ' ') : ''}</h1>
 				</div>
 
 				<div className='product-button-container'>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'stairs';
-							console.log(state.selected);
-						}}
-					>
-						Stairs
-					</button>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'doors';
-							console.log(state.selected);
-						}}
-					>
-						Doors
-					</button>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'outer_walls';
-							console.log(state.selected);
-						}}
-					>
-						Outer Walls
-					</button>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'inner_walls';
-							console.log(state.selected);
-						}}
-					>
-						Inner Walls
-					</button>
-
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'floor';
-							console.log(state.selected);
-						}}
-					>
-						Floor
-					</button>
-				</div>
-				<div className='product-button-container'>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'windows';
-							console.log(state.selected);
-						}}
-					>
-						Window
-					</button>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'roof';
-							console.log(state.selected);
-						}}
-					>
-						Roof
-					</button>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'balcony';
-							console.log(state.selected);
-						}}
-					>
-						Balcony
-					</button>
-					<button
-						className='product-button'
-						onClick={() => {
-							state.selected = 'fence';
-							console.log(state.selected);
-						}}
-					>
-						Fence
-					</button>
+					{meshList.map((mesh) => {
+						return (
+							<button
+								className='product-button'
+								onClick={() => {
+									state.selected = mesh.name;
+								}}
+							>
+								{mesh.name.replace('_', ' ')}
+							</button>
+						);
+					})}
 					<button
 						className='product-button'
 						onClick={() => {
 							state.selected = null;
-							console.log(state.selected);
 						}}
 					>
-						Reset
+						RESET
 					</button>
 				</div>
 			</div>
