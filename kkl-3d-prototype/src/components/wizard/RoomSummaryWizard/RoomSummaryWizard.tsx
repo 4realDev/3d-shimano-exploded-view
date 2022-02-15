d1a2062da46939faa689335ed464a12e171a5441
Vladimir Brazhnik <v.brazhnik@web.de>
2022-02-15 13:26:09 +0100

    Add EditButton component and use it in RoomSummaryWizard and RoomCard

--- kkl-3d-prototype/src/components/wizard/RoomSummaryWizard/RoomSummaryWizard.tsx
+++ kkl-3d-prototype/src/components/wizard/RoomSummaryWizard/RoomSummaryWizard.tsx
@@ -2,8 +2,10 @@ import { TextField } from '@mui/material';
 import { useEffect, useMemo } from 'react';
 import { roomList } from '../../../data/roomData';
 import { setFilteredMeshes, setSelectedMeshes, showRoomsOverview } from '../../../store/useCameraStore';
-import { ROOM_TYPE, WizardDataType } from '../../../store/useWizardStore';
+import { ROOM_TYPE, setStep, WizardDataType } from '../../../store/useWizardStore';
 import { getEventTypeText } from '../../../utils/room';
+import Edit from '../../icons/Edit';
+import EditButton from '../../ui/EditButton/EditButton';
 import RoomCard from '../../ui/RoomCard/RoomCard';
 import styles from './RoomSummaryWizard.module.scss';
 
@@ -78,6 +80,20 @@ const RoomSummaryWizard = ({ handleChange, wizardData }: RoomSummaryWizardProps)
 					equipment={chosenWizardMainRoomData.equipment}
 					chairFormation={chosenWizardMainRoomData.chair_formation}
 					roomType={ROOM_TYPE.mainRooms}
+					editButton={
+						<>
+							<EditButton label='Hauptraum anpassen' onClick={() => setStep(1)} />
+							{chosenSideRoomInfoObject === undefined && chosenWizardSideRoomData === undefined && (
+								<EditButton
+									label='Zusätzlichen Nebenraum auswählen'
+									onClick={() => {
+										setStep(2);
+										handleChange(true, 'additionalRooms');
+									}}
+								/>
+							)}
+						</>
+					}
 				/>
 			)}
 
@@ -95,11 +111,12 @@ const RoomSummaryWizard = ({ handleChange, wizardData }: RoomSummaryWizardProps)
 					equipment={chosenWizardSideRoomData.equipment}
 					chairFormation={chosenWizardSideRoomData.chair_formation}
 					roomType={ROOM_TYPE.sideRooms}
+					editButton={<EditButton label='Nebenraum anpassen' onClick={() => setStep(2)} />}
 				/>
 			)}
 
 			<div className={styles.summaryCard}>
-				<h3 className={styles.summaryCard__title}>Kriterien</h3>
+				<h3 className={styles.summaryCard__title}>Filterkriterien</h3>
 				<div className={styles.summaryCard__item}>
 					<div className={styles.summaryCard__item__key}>Art des Events: </div>
 					<div className={styles.summaryCard__item__value}>{getEventTypeText(wizardData.eventType)}</div>
@@ -121,6 +138,9 @@ const RoomSummaryWizard = ({ handleChange, wizardData }: RoomSummaryWizardProps)
 					<div className={styles.summaryCard__item__value}>
 						{wizardData.endDate ? formatDate(wizardData.endDate) : 'undefiniert'}
 					</div>
+				</div>
+				<div style={{ marginTop: 8 + 'px' }}>
+					<EditButton label='Filterkriterien anpassen' onClick={() => setStep(0)} />
 				</div>
 			</div>
 
