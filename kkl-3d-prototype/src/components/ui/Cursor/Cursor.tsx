307c9e77ea69ae129b3c9d7ddbbbe4b9200283f1
Vladimir Brazhnik <v.brazhnik@web.de>
2022-02-16 19:56:18 +0100

    Simplify Cursor by removing unnecessary useEffect

--- kkl-3d-prototype/src/components/ui/Cursor/Cursor.tsx
+++ kkl-3d-prototype/src/components/ui/Cursor/Cursor.tsx
@@ -1,17 +1,15 @@
-import { useEffect } from 'react';
 import { useCameraStore } from '../../../store/useCameraStore';
 import { getRoomTitleByMeshName } from '../../../utils/room';
 
 const Cursor = () => {
 	const hoveredMesh = useCameraStore((state) => state.hoveredMesh);
-	useEffect(() => {
-		const hoveredRoomName = hoveredMesh && getRoomTitleByMeshName(hoveredMesh);
-		// ASSUMPTION: char width ~ 6px | margin-right ~ 30px
-		const rectWidth = hoveredRoomName ? hoveredRoomName.length * 6 + 30 : 0;
-		const fontSize = rectWidth > 128 ? 10 : 12;
+	const hoveredRoomName = hoveredMesh && getRoomTitleByMeshName(hoveredMesh);
+	// ASSUMPTION: char width ~ 6px | margin-right ~ 30px
+	const rectWidth = hoveredRoomName ? hoveredRoomName.length * 6 + 30 : 0;
+	const fontSize = rectWidth > 128 ? 10 : 12;
 
-		// NOTE: Limit of cursor size is 128×128px. Larger cursor images are ignored
-		const cursor = `
+	// NOTE: Limit of cursor size is 128×128px. Larger cursor images are ignored
+	const cursor = `
 		<svg width="128" height="64" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/>
 			<rect
@@ -28,12 +26,11 @@ const Cursor = () => {
 			</text>
 		</svg>`;
 
-		if (document.getElementById('canvas')) {
-			document.getElementById('canvas')!!.style.cursor = `url('data:image/svg+xml;base64,${Buffer.from(cursor).toString(
-				'base64'
-			)}'), auto`;
-		}
-	}, [hoveredMesh]);
+	if (document.getElementById('canvas')) {
+		document.getElementById('canvas')!!.style.cursor = `url('data:image/svg+xml;base64,${Buffer.from(cursor).toString(
+			'base64'
+		)}'), auto`;
+	}
 
 	return null;
 };
